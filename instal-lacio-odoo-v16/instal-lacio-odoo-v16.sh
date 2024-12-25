0a69302ee79b0c67bad32127a7aacc342725a4ab
llopfilms <llopfilms@gmail.com>
2024-12-25 23:52:59 +0100

    Actualització

--- instal-lacio-odoo-v16/instal-lacio-odoo-v16.sh
+++ instal-lacio-odoo-v16/instal-lacio-odoo-v16.sh
@@ -73,7 +73,7 @@ function prompt_required {
 function prompt_yes_no {
   local prompt_text=$1
   local default_value=$2
-  read -p "$prompt_text [$default_value]:" input_value
+  read -p "$prompt_text: [$default_value]" input_value
   input_value=${input_value:-$default_value}#!/bin/bash
 }
 
