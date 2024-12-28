bf0a125b477639ccba713b1c4624360d94b87b1d
llopfilms <llopfilms@gmail.com>
2024-12-28 15:08:24 +0100

    Actualització

--- instal-lacio-odoo-v16/instal-lacio-odoo-v16.sh
+++ instal-lacio-odoo-v16/instal-lacio-odoo-v16.sh
@@ -760,9 +760,9 @@ server {
 
     location / {
         proxy_pass http://odoo16;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto https;
     }
 }
